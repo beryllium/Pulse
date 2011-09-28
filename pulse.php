56a093be4a2ac5e086b64a09119ad4b680c9a74a
Kevin Boyd <kevin.boyd@gmail.com>
2011-09-27 21:25:21 -0700

    Initial version.

---
+++ pulse.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Pulse 1.0
+ *
+ * Generates an encrypted array of information intended to provide an update on the health of a server.
+ *
+ * In order to fully use it, you must first give values to the Keymaster and Gatekeeper variables.
+ *
+ * Usage example:
+ * 		$data = file_get_contents( 'http://slimer.example.com/pulse.php?gatekeeper=ZUUL' );
+ *
+ * 		$array = unserialize( mcrypt_decrypt( 
+ * 		             MCRYPT_RIJNDAEL_256, 
+ * 		             'my secret keymaster value which is of course STAY PUFT MARSHMALLOW MAN',
+ * 		             $data,
+ * 		             MCRYPT_MODE_ECB );
+ *
+ * 		print_r( $array );
+ *
+ * @author Kevin Boyd (aka Beryllium) <beryllium@beryllium.ca> http://www.beryllium.ca/
+ */
+
+//$keymaster = ''; //Set this to a secret key that only your monitoring software knows (do NOT pass it by URL)
+//$gatekeeper = ''; //Set this to a semi-secret key that can be passed to the script to extract the complex data.
+
+//If gatekeeper is not provided, only a simple text message will be returned (useful for Amazon ELB health checks).
+
+header( 'Content-type: text/plain' );
+if ( empty( $keymaster ) || empty( $gatekeeper ) )
+{
+	echo "Pulse script is not configured.\n\nAre you the Keymaster?";
+	exit;
+}
+
+if ( $gatekeeper != $_REQUEST[ 'gatekeeper' ] )
+{
+	echo "Server is up.\n\nAre you the Gatekeeper?";
+	exit;
+}
+
+if ( !extension_loaded( 'mcrypt' ) )
+{
+	echo "MCrypt not loaded.\n\nWho ya gonna call?";
+	exit;
+}
+
+$status_array = @array();
+
+$status_array[ 'pulse_version' ] = '1.0';
+$status_array[ 'server_status' ] = 'up';
+$status_array[ 'load_avg' ] = sys_getloadavg(); //this won't work on Windows platforms
+$status_array[ 'date' ] = date( 'Y-m-d H:i:s' );
+$status_array[ 'phpversion' ] = phpversion();
+$status_array[ 'gc' ] = gc_enabled();
+$status_array[ 'resource_usage' ] = getrusage(); //again, won't work on Windows
+$status_array[ 'uname' ] = php_uname();
+$status_array[ 'extensions' ] = get_loaded_extensions();
+$status_array[ 'diskfree' ] = disk_free_space( '.' );
+$status_array[ 'disktotal' ] = disk_total_space( '.' );
+
+$packet = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $keymaster, serialize( $status_array ), MCRYPT_MODE_ECB );
+$packet = base64_encode( $packet );
+
+echo $packet;
